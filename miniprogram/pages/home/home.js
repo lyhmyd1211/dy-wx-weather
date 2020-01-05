050ed9a9bd84f9540dffce3abe33fac8787d810b
lyhmyd1211 <lyhwyx@vip.qq.com>
2020-01-05 18:41:59 +0800

    fix

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,66 +1,45 @@
-// miniprogram/pages/home/home.js
+import * as echarts from '../../ec-canvas/echarts';
+import   '../../ec-canvas/echarts-liquidfill.min'
+let chart = null;
+
+// 2、进行初始化数据
+function initChart(canvas, width, height) {
+  chart = echarts.init(canvas, null, {
+    width: width,
+    height: height
+  });
+  canvas.setChart(chart);
+
+  var option = {
+    series: [{
+      type: 'liquidFill',
+      data: [0.6]
+  }]
+  };
+
+  chart.setOption(option);
+  return chart;
+}
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  onShareAppMessage: function(res) {
+    return {
+      title: 'ECharts',
+      path: '/pages/index/index',
+      success: function() {},
+      fail: function() {}
+    };
+  },
   data: {
-
+    ec: {
+      onInit: initChart // 3、将数据放入到里面
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onReady() {
+    setTimeout(function() {
+      // 获取 chart 实例的方式
+      console.log(chart);
+    }, 2000);
   }
-})
+});
